9e8748ed686bc136871860fe18ba729e3bd45562
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-04-09 12:45:04 -0300

    added postgres

--- app/config/database.js
+++ app/config/database.js
@@ -1,15 +1,21 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+		  defaultConnection: 'default',
+		  connections: {
+				      default: {
+							        connector: 'bookshelf',
+							        settings: {
+											        client: 'postgres',
+											        host: env('DATABASE_HOST', 'localhost'),
+											        port: env.int('DATABASE_PORT', 5432),
+											        database: env('DATABASE_NAME', 'strapi'),
+											        username: env('DATABASE_USERNAME', 'strapi'),
+											        password: env('DATABASE_PASSWORD', 'strapi'),
+											        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+											        ssl: false,											      },
+							        options: {
+											        ssl: env.bool('DATABASE_SSL', false),
+											      },
+							      },
+				    },
 });
+
